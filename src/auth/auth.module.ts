4313249135934b076a4014d4704a767aad93c3e4
mehran.saghebifard <m.saghebi.dev@gmail.com>
2025-01-20 13:17:40 +0330

    working on user register

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,22 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/schema/user.model';
 
 @Module({
   imports: [
     PassportModule,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+
     JwtModule.register({
       secret: 'SECRET_KEY',
       signOptions: { expiresIn: '1h' },
     }),
   ],
+  controllers: [AuthController],
+
   providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
