7a252358fc2f15bb9a2f542f892809c49d0f9764
mehran.saghebifard <m.saghebi.dev@gmail.com>
2025-01-20 13:54:24 +0330

    wip : login works :fire:

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,28 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/schema/user.model';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
 
-  async validateUser(username: string, password: string): Promise<any> {
-    // Replace this with your User model logic
-    const user = { username: 'test', password: '12345', role: 'admin' };
-    if (user && user.password === password) {
-      const { password, ...result } = user;
-      console.log(password);
+  async validateUser(username: string, password: string): Promise<User | null> {
+    const user = await this.userModel.findOne({ username });
 
-      return result;
+    // If user doesn't exist
+    if (!user) {
+      return null;
     }
-    return null;
-  }
 
+    // Compare entered password with the hashed password in DB
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    // If passwords do not match, return null
+    if (!isPasswordValid) {
+      return null;
+    }
+
+    // Return the user if the password is correct
+    return user;
+  }
   async login(user: any) {
     const payload = {
       username: user.username,
       sub: user.userId,
       role: user.role,
     };
+
+    this.validateUser(user.username, user.password);
     return {
       access_token: this.jwtService.sign(payload),
     };
